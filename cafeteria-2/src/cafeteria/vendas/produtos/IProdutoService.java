c3538cd4ce86756ce7e8efd750c46bccb2bee6c4
186625 <151871920+186625@users.noreply.github.com>
2024-11-21 13:44:53 -0300

    Update IProdutoService.java

--- cafeteria-2/src/cafeteria/vendas/produtos/IProdutoService.java
+++ cafeteria-2/src/cafeteria/vendas/produtos/IProdutoService.java
@@ -1,5 +1,8 @@
 package cafeteria.vendas.produtos;
 
 public interface IProdutoService {
+    Produto pesquisarProdutoPorId(int idProduto);
+    void cadastrarProduto (String nome, int id, UnidadeMedida medida, double preco);
+    void atualizarProduto(String nome, int id, UnidadeMedida medida, double preco);
 
 }
